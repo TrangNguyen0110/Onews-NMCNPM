fbddacb28f98240d007525b82c2f0fa5805645e9
dongsonbk58 <20133319@student.hust.edu.vn>
2017-11-03 00:38:18 +0700

    add some fragment for all tab and some models

--- Onews-mobile-android/Onews/app/src/main/java/com/example/dongson/onews/view/MainFragment.java
+++ Onews-mobile-android/Onews/app/src/main/java/com/example/dongson/onews/view/MainFragment.java
@@ -19,17 +19,27 @@ public class MainFragment extends Fragment {
     public MainFragment() {
     }
 
+<<<<<<< HEAD
     public static MainFragment newInstance() {
         MainFragment fragment = new MainFragment();
         return fragment;
     }
 
+=======
+>>>>>>> add some fragment for all tab and some models
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+<<<<<<< HEAD
 //        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
 //        textView.setText("Kteam");
+=======
+        FrameLayout relativeLayout = (FrameLayout) rootView.findViewById(R.id.rl_fragment);
+        relativeLayout.setBackgroundColor(Color.RED);
+        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+        textView.setText("Kteam");
+>>>>>>> add some fragment for all tab and some models
         return rootView;
     }
 }
